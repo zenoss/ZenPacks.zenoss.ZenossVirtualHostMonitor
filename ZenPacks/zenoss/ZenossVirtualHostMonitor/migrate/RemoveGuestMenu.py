0972f75ebe43bd0cbd545bc4614cf123fc5d5f53
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-10-22 01:08:52 +0000

    * Fixes #7445: add in missing migration script. Originally reviewed by Chip.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.ZenossVirtualHostMonitor@36616 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/ZenossVirtualHostMonitor/migrate/RemoveGuestMenu.py
@@ -0,0 +1,35 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+import logging
+log = logging.getLogger('zenpack.ZenossVirtualHostMonitor')
+
+import Globals
+from Products.ZenModel.migrate.Migrate import Version
+from Products.ZenModel.ZenPack import ZenPack
+
+class RemoveGuestMenu:
+    version = Version(3, 1, 0)
+
+    def migrate(self, pack):
+        log.info('Removing Guest menu item')
+        dmd = pack.__primary_parent__.__primary_parent__
+        id = 'virtualMachineDetail'
+        moreMenuIds = [menu.id for menu in dmd.zenMenus.More.objectValues()]
+        if id in moreMenuIds:
+            try:
+                dmd.zenMenus.More.manage_deleteZenMenuItem((id,))
+            except (KeyError, AttributeError):
+                pass
+                     
+RemoveGuestMenu()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.ZenossVirtualHostMonitor'
-VERSION = '2.3.0'
+VERSION = '2.3.1'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
