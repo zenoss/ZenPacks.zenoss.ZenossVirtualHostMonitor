53f86d28dcbf7f39af341ddd1b2d78ed4a3802e2
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:16:47 +0000

    [trunk/core] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.ZenossVirtualHostMonitor@61801 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- GNUmakefile
+++ GNUmakefile
@@ -1,8 +1,12 @@
-######################################################################
-#
-# Copyright 2007 Zenoss, Inc.  All Rights Reserved.
-#
-######################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
 
 default: egg
 
--- ZenPacks/zenoss/ZenossVirtualHostMonitor/migrate/ConvertVirtualHosts.py
+++ ZenPacks/zenoss/ZenossVirtualHostMonitor/migrate/ConvertVirtualHosts.py
@@ -1,15 +1,12 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2009, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 or (at your
-# option) any later version as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2009, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
 
 import Globals
 from Products.ZenModel.migrate.Migrate import Version
@@ -63,4 +60,3 @@ class ConvertVirtualHosts(ZenPackMigration):
             # devices in the device class (internal #1394)
             if isinstance( dev, VirtualMachineHost ):             
                 self.convert(dev.guestDevices(), oldClass, VirtualMachine)
-        
